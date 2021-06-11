b152858a10822e43143da02010e8c9f7f122f54c
Daseul <dls2tmfs2@gmail.com>
2021-06-11 18:10:52 +0900

    style: rending페이지 레이아웃/fade효과

--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Main from './pages/Main';
+import Rending from './pages/Rending';
 import Mypage from './pages/Mypage/Mypage';
 
 export default function Routes() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={Main}></Route>
+        <Route exact path="/" component={Rending}></Route>
         <Route exact path="/mypage" component={Mypage}></Route>
       </Switch>
     </Router>
