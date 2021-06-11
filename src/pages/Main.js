c0483d451363a8fc16c2ade8616c61b04884e43f
Daseul <dls2tmfs2@gmail.com>
2021-06-12 02:01:24 +0900

    misc: 다른브랜치 수정으로 인한 백업

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -4,10 +4,11 @@ import styled from 'styled-components';
 const Main = () => {
   return (
     <Container>
+      <TimeDescription>지금은 6월 9일</TimeDescription>
+      <TimeDescription>오후 4시 30분 입니다.</TimeDescription>
       <TimeSection>
-        <TimeDescription>지금은 6월 9일</TimeDescription>
-        <TimeDescription>오후 4시 30분 입니다.</TimeDescription>
-        <StartTime>이다슬님은 09시 30분에 시작하셨습니다!</StartTime>
+        <StudentName>이다슬</StudentName>
+        <StartTime>님은 09시 30분에 시작하셨습니다!</StartTime>
       </TimeSection>
     </Container>
   );
@@ -29,6 +30,12 @@ const TimeDescription = styled.h1`
 `;
 
 const StartTime = styled.h2`
+  color: ${({ theme }) => theme.colors.fontColor};
+  font-size: 40px;
+  font-weight: 500;
+`;
+
+const StudentName = styled.h2`
   color: ${({ theme }) => theme.colors.fontColor};
   font-size: 40px;
   font-weight: 500;
