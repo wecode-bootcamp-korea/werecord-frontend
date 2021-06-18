789ce58bf1d54dc93c1fbc75550e95f336ec63f3
SUYEON <gsy4568@gmail.com>
2021-06-18 21:27:24 +0900

    feat : 회원정보 수정 기능 구현 완료

--- src/pages/Mypage/EditForm.js
+++ src/pages/Mypage/EditForm.js
@@ -1,17 +1,74 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import styled from 'styled-components';
 
-export default function EditContents() {
+export default function EditContents({ handleModal }) {
   const [userForm, setUserForm] = useState({
     name: '',
-    image: '',
+    user_type: '',
+    batch: '',
     position: '',
     blog: '',
     github: '',
-    beginDate: '',
-    endDate: '',
-    birthDay: '',
+    birthday: '',
+    profile_image_url: [],
   });
+  const [userId, setUserId] = useState('');
+  const [imgFile, setImgFile] = useState('');
+  const { name, position, blog, github, birthday } = userForm;
+  const history = useHistory();
+
+  useEffect(() => {
+    fetch('', {
+      headers: {
+        Authorization: '',
+      },
+    })
+      .then(res => res.json())
+      .then(userData => {
+        const {
+          name,
+          batch,
+          position,
+          blog,
+          github,
+          birthday,
+          user_type,
+          user_id,
+        } = userData.data;
+
+        setUserForm(prev => ({
+          ...prev,
+          name,
+          batch,
+          position,
+          blog,
+          github,
+          birthday,
+          user_type,
+        }));
+        setUserId(user_id);
+      });
+  }, []);
+
+  const modifyUserData = e => {
+    e.preventDefault();
+    const userInfo = JSON.stringify(userForm);
+    const userData = new FormData();
+    userData.append('info', userInfo);
+    userData.append('image', imgFile);
+
+    fetch(`/${userId}`, {
+      method: 'POST',
+      header: {
+        Authorization: '',
+      },
+      body: userData,
+    });
+
+    history.push('/mypage');
+    handleModal(false);
+  };
 
   const handleInput = e => {
     const { name, value } = e.target;
@@ -19,7 +76,16 @@ export default function EditContents() {
     setUserForm(nextUserForm);
   };
 
-  console.log(userForm);
+  const onFileInput = e => {
+    e.preventDefault();
+    const reader = new FileReader();
+    const file = e.target.files[0];
+    reader.readAsDataURL(file);
+
+    reader.onload = () => {
+      setImgFile(file);
+    };
+  };
 
   return (
     <Container>
@@ -29,6 +95,7 @@ export default function EditContents() {
         <Input
           name="name"
           placeholder="이름을 입력해주세요"
+          value={name || ''}
           onChange={handleInput}
           required
         />
@@ -38,8 +105,9 @@ export default function EditContents() {
         <Input
           name="image"
           type="file"
+          accept="image/*"
           placeholder="이미지 주소로 입력해주세요!"
-          onChange={handleInput}
+          onChange={onFileInput}
           required
         />
       </Content>
@@ -47,6 +115,7 @@ export default function EditContents() {
         <Label>포지션</Label>
         <Input
           name="position"
+          value={position || ''}
           placeholder="Front, Back"
           onChange={handleInput}
           required
@@ -56,6 +125,7 @@ export default function EditContents() {
         <Label>blog 주소</Label>
         <Input
           name="blog"
+          value={blog || ''}
           placeholder="개인 블로그 주소"
           onChange={handleInput}
           required
@@ -65,25 +135,8 @@ export default function EditContents() {
         <Label>github 주소</Label>
         <Input
           name="github"
+          value={github || ''}
           placeholder="Github 주소"
-          onChange={handleInput}
-          required
-        />
-      </Content>
-      <Content>
-        <Label>위코드 시작일</Label>
-        <Input
-          name="beginDate"
-          placeholder="ex) 2021.06.15"
-          onChange={handleInput}
-          required
-        />
-      </Content>
-      <Content>
-        <Label>위코드 종료일</Label>
-        <Input
-          name="endDate"
-          placeholder="ex) 2021.06.15"
           onChange={handleInput}
           required
         />
@@ -91,13 +144,14 @@ export default function EditContents() {
       <Content>
         <Label>생일</Label>
         <Input
-          name="birthDay"
-          placeholder="1993.06.05"
+          name="birthday"
+          value={birthday || ''}
+          placeholder="04.12"
           onChange={handleInput}
           required
         />
       </Content>
-      <SubmitBtn>수정</SubmitBtn>
+      <SubmitBtn onClick={modifyUserData}>수정</SubmitBtn>
     </Container>
   );
 }
